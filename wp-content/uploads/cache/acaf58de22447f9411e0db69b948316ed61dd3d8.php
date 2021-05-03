478673b7ee4e0e1ef9af9fe36de0c5a0ce15f006
Phil Smith <phil@amillionmonkeys.co.uk>
2021-05-03 19:01:07 +0100

    fixes

--- wp-content/uploads/cache/acaf58de22447f9411e0db69b948316ed61dd3d8.php
+++ wp-content/uploads/cache/acaf58de22447f9411e0db69b948316ed61dd3d8.php
@@ -2,7 +2,7 @@
             <div class="m-auto max-w-screen-2xl p-4 py-2 lg:flex justify-between items-center">
                 <div class=" sm:mb-4 lg:mb-0">
                     <p>
-                        <a href="/privacy-policy" class="inline-block mr-2">
+                        <a href="/privacy-policy" class="inline-block footer-item">
                             Privacy Policy
                         </a>
                         <a href="/cookie-policy-and-control/">
