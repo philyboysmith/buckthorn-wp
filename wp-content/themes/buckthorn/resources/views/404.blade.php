d4e37971f3b2022017c998411cc3dd15c212fec9
Phil Smith <phil@amillionmonkeys.co.uk>
2021-05-11 08:35:21 +0100

    fixes

--- wp-content/themes/buckthorn/resources/views/404.blade.php
+++ wp-content/themes/buckthorn/resources/views/404.blade.php
@@ -1,12 +1,21 @@
 @extends('layouts.app')
 
 @section('content')
-  @include('partials.page-header')
+  @while(have_posts()) @php the_post() @endphp
+  <main class="site-content text-grey text-lg"  >
 
-  @if (!have_posts())
-    <div class="alert alert-warning">
-      {{ __('Sorry, but the page you were trying to view does not exist.', 'sage') }}
-    </div>
-    {!! get_search_form(false) !!}
-  @endif
+            <div class="white-opacity-strip">
+            </div>
+            <div class="mx-auto max-w-screen-2xl p-6 py-12 mb-12 md:text-lg bg-white-trans">
+                <div class="flex flex-wrap">
+                    <div class="w-full pl-6 border-l-4 border-blue">
+
+                        <div class="prose">
+                      @php get_the_content(517) @endphp
+                        </div>
+                    </div>
+                </div>
+            </div>
+    </main>
+  @endwhile
 @endsection
--- wp-content/themes/buckthorn/resources/views/single.blade.php
+++ wp-content/themes/buckthorn/resources/views/single.blade.php
@@ -1,7 +1,41 @@
 @extends('layouts.app')
 
 @section('content')
-  @while(have_posts()) @php the_post() @endphp
-    @include('partials.content-single-'.get_post_type())
-  @endwhile
+<main class="site-content text-grey text-lg">
+    <picture>
+        <source media="(orientation: landscape)" srcset="{{ the_field('desktop')}}">
+        <source media="(orientation: portrait)" srcset="{{ the_field('portrait')}}">
+        <img src="{{ the_field('desktop')}}" alt="" class="w-full h-full object-cover fixed  inset-0" style="z-index: -1000">
+    </picture>
+    <div class="white-opacity-strip mb-4 md:mb-12"></div>
+    <div class="mx-auto max-w-screen-2xl">
+        <div class="p-6 md:py-12 m-4 mb-12 md:text-lg bg-white-trans">
+            <div class="lg:flex flex-wrap mb-12">
+                <div class="mb-12 lg:mb-0">
+
+                    <div class="lg:flex items-start">
+                        <div class="w-full text-lg lg:w-full mb-12 lg:mb-0 lg:pr-24">
+                            @while (have_posts()) @php the_post() @endphp
+                            <div class="w-full bg-white border-l-4 border-dgreen p-4 pl-6 mb-4 repo-panel relative">
+                            <a class="absolute inset-0 z-10" href="{{the_field('file')}}">&nbsp;</a>
+                                    <h3 class="font-serif font-semibold text-2xl lg:text-3xl">{!! get_the_title() !!}</h3>
+                                <div class="w-11/12">
+                                    <div class="font-semibold text-sm">
+                                        <p class="mb-1"><time class="inline-block updated" datetime="{{ get_post_time('c', true) }}" >{{ get_the_date() }}</time></p>
+                                    </div>
+                                    <p class="mb-0">{!! get_the_content() !!}</p>
+                                </div>
+                                <div class="w-1/12"><img alt="" src="/assets/images/icon-pdf-dgreen.svg" class="w-12"></div>
+                            </div>
+                            @endwhile
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
+</main>
+
 @endsection
