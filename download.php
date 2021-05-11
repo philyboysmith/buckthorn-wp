d9ed3a3c326e97e90505242348a183ee8311b660
Phil Smith <phil@amillionmonkeys.co.uk>
2021-05-11 10:29:00 +0100

    fixes

---
+++ download.php
@@ -0,0 +1,19 @@
+<?php
+
+if(!empty($_GET['file'])){
+    $fileName = basename($_GET['file']);
+    $filePath = 'wp-content/uploads/documents/'.$fileName;
+    if(!empty($fileName) && file_exists($filePath)){
+        // Define headers
+        header("Cache-Control: public");
+        header("Content-Description: File Transfer");
+        header("Content-Disposition: attachment; filename=$fileName");
+        header("Content-Type: application/pdf");
+
+        // Read the file
+        readfile($filePath);
+        exit;
+    }else{
+        echo 'The file does not exist.';
+    }
+}
